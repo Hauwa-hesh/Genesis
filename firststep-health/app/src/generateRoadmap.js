351918ab66166b2b613534cf8e9276593e2631c3
nafisa-bashir <123766536+nafisa-bashir@users.noreply.github.com>
2026-06-26 21:10:47 +0400

    Update generateRoadmap.js

--- firststep-health/app/src/generateRoadmap.js
+++ firststep-health/app/src/generateRoadmap.js
@@ -1,1 +1,7 @@
+import sampleResult from './sampleResult.json';
+
+export async function generateRoadmap(formData) {
+  console.log('Mock roadmap generated for:', formData);
+  return sampleResult;
+}
 
